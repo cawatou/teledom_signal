a28589243c88a4d6b4aedd2ce787fdf75838e705
cawatou <cawatou@gmail.com>
2018-04-10 17:27:33 +0300

    domofon <-> mobile (p1)

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-var u, b, v, r, l;
-
-var w = window,
-	PeerConnection = w.mozRTCPeerConnection || w.webkitRTCPeerConnection,
-	SessionDescription = w.mozRTCSessionDescription || w.RTCSessionDescription,
-	IceCandidate = w.mozRTCIceCandidate || w.RTCIceCandidate;
-
+var sc = io('https://skipodev.ru:8011');
 var peerConnectionConfig = {
   'iceServers': [
     {'urls': 'stun:stun.l.google.com:19302'},
     //{'urls': 'turn:185.22.235.182:3478', 'credential': 'teledom', 'username': 'teledom'}
   ]
 };
-
-var constrains = {
-	'optional':
-		[{'DtlsSrtpKeyAgreement': true}, {'RtpDataChannels': true }]
-};
-
-var pc, channel;
-var sc = io('https://skipodev.ru:8011');
+var uuid, pc, channel;
 
 function createPC(isOffer) {
-	u = createUUID();
+	uuid = createUUID();
 	pc = new RTCPeerConnection(peerConnectionConfig);
 	console.log('createPC: ', pc);
 	
 	// send any ice candidates to the other peer
 	pc.onicecandidate = function (e) {
 		console.log('onicecandidate', e.candidate);
-		if(e.candidate != null) sc.emit("candidate", { ice: e.candidate, uuid: u });
+		if(e.candidate != null) sc.emit("candidate", { ice: e.candidate, uuid: uuid });
 	};
 
 	pc.onconnection = function() {
-		// Пока это срабатывает только в Firefox
 		console.log('Connection established');
 	};
 	pc.onclosedconnection = function() {
-		// И это тоже. В Chrome о разрыве соединения придется узнавать другим способом
 		console.log('Disconnected');
 	};
 
@@ -55,11 +40,12 @@ sc.on('candidate', function(data) {
 });
 
 function openOfferChannel(pc) {
-	// Первый параметр – имя канала, второй - настройки. В настоящий момент Chrome поддерживает только UDP-соединения (non-reliable), а Firefox – и UDP, и TCP (reliable)
+	// Первый параметр – имя канала, второй - настройки.
 	channel = pc.createDataChannel('RTCDataChannel', {});
 	console.log('openOfferChannel', channel);
 	setChannelEvents();
 }
+
 function setChannelEvents() {
 	
 	channel.onopen = function() {
@@ -83,7 +69,7 @@ function createOffer(pc) {
 			console.log('setLocalDescription', pc);
 			console.log('send offer: ', offer);
 			// Отправляем другому участнику через сигнальный сервер
-			sc.emit("offer", { sdp: pc.localDescription, uuid: u });
+			sc.emit("offer", { sdp: pc.localDescription, uuid: uuid });
 			// После завершения этой функции начнет срабатывать событие pc.onicecandidate
 		}, function(err) {
 			console.log('Failed to setLocalDescription():', err);
@@ -92,18 +78,6 @@ function createOffer(pc) {
 		console.log('Failed to createOffer():', err);
 	});
 }
-
-
-
-// var sdpHandler = msg => pc.setRemoteDescription(new RTCSessionDescription(msg.sdp)).then(() => {
-// 	if (msg.sdp.type == "offer") {
-// 		pc.createAnswer().then(answer => {
-// 			pc.setLocalDescription(answer).then(() => {
-// 				sc.emit("answer", { sdp: pc.localDescription, uuid: u })
-// 			}).catch(log);
-// 		}).catch(log);
-// 	}
-// }).catch(log);
 
 function setRemoteSDP(msg) {
 	console.log('setRemoteSDP', msg);
@@ -122,7 +96,6 @@ sc.on('offer', function(sdp) {
 	console.log('Пришел Offer от другого участника', sdp);
 	if(!pc) {
 		// Пришел Offer от другого участника
-		// Боб создает соединение
 		createPC(false);
 	}
 	setRemoteSDP(sdp);
@@ -132,8 +105,6 @@ function openAnswerChannel(pc) {
 	console.log('openAnswerChannel', pc);
 	pc.ondatachannel = function(e) {
 		channel = e.channel;
-		//channel.binaryType = 'blob';
-		console.log('ondatachannel', channel);
 		setChannelEvents();
 	};
 }
@@ -143,7 +114,7 @@ function createAnswer() {
 	pc.createAnswer(function(offer) {
 		pc.setLocalDescription(offer, function() {
 			// Отправляем другому участнику через сигнальный сервер
-			sc.emit("answer", { sdp: pc.localDescription, uuid: u })
+			sc.emit("answer", { sdp: pc.localDescription, uuid: uuid })
 		}, function(err) {
 			console.log('Failed to setLocalDescription():', err);
 		});
@@ -151,25 +122,16 @@ function createAnswer() {
 		console.log('Failed to createAnswer():', err);
 	});
 }
-sc.on('offer', function(sdp) {
-	console.log('Пришел Offer от другого участника', sdp);
-	if(!pc) {
-		// Пришел Offer от другого участника
-		// Боб создает соединение
-		createPC(false);
-	}
-	setRemoteSDP(sdp);
-});
+
 sc.on("answer", function(data){
 	setRemoteSDP(data);	
 })
 
-/*function send(){
+function send(text){
 	console.log('send()');
 	channel.send("Hi there!");
-	console.log('pc: ', pc, 'channel: ', channel);
-}*/
-
+	//console.log('pc: ', pc, 'channel: ', channel);
+}
 
 // Strictly speaking, it's not a real UUID, but it gets the job done here
 function createUUID() {
